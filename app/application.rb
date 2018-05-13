7d9e6d259b528f7d02e7190b8152b3f439be7bcc
Arye Dov Eidelman <aryedov.e@gmail.com>
2018-05-13 18:27:38 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,8 +3,14 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
+    case req.path
+    when /\/testing/
+      resp.write 'Route not found'
+      resp.status = 404
+    # when condition
+      
+    end
     resp.write ''
-    resp.status = 200
     resp.finish
   end
 end
